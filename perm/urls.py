da0d0c2fbf7bb54559697ada55eba8cf69632b6d
Unknown <josselin.pennors@hotmail.fr>
2019-09-01 22:57:24 +0200

    Add methods to deal with articles and sales

--- perm/urls.py
+++ perm/urls.py
@@ -4,5 +4,7 @@ from rest_framework import routers
 from perm import views as perm_views
 
 urlpatterns = [
-
+	path('payutc/article/<int:id>', perm_views.create_payutc_article),
+	path('sales/article/<int:id>', perm_views.get_article_sales),
+	path('sales/<int:id>', perm_views.get_creneau_sales)
 ]
--- perm/views.py
+++ perm/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import api_view, permission_classes
+from django.http import JsonResponse
 from rest_framework import viewsets
 from . import serializers as perm_serializers
 from .import models as perm_models
@@ -15,6 +17,14 @@ class CreneauViewSet(viewsets.ModelViewSet):
     queryset = perm_models.Creneau.objects.all()
     permission_classes = (IsMemberUserOrReadOnly,)
 
+
+@api_view(['GET'])
+@permission_classes((IsMemberUser, ))
+def get_creneau_sales(request, id):
+    c = perm_models.Creneau.objects.get(pk=id)
+    return JsonResponse(c.get_justificatif_information())
+
+
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = perm_serializers.ArticleSerializer
     queryset = perm_models.Article.objects.all()
@@ -24,3 +34,28 @@ class SignatureViewSet(viewsets.ModelViewSet):
     serializer_class = perm_serializers.SignatureSerializer
     queryset = perm_models.Signature.objects.all()
     permission_classes = (IsAdminUser,)
+
+
+@api_view(['GET'])
+@permission_classes((IsMemberUser, ))
+def create_payutc_article(request, id):
+    # Endpoint qui permet d'obtenir, pour un article de pk {id}, d'enregistrer l'article dans PayUTC.
+    article = perm_models.Article.objects.get(pk=id)
+    sessionid = request.session['payutc_session']
+    article.create_payutc_article(sessionid)
+    # TVConfiguration.objects.create(tv_id=1, url="http://beethoven.picasso-utc.fr/NextMenus", enable_messages=False)
+    # TVConfiguration.objects.create(tv_id=2, url="http://beethoven.picasso-utc.fr/NextMenus", enable_messages=False)
+    return JsonResponse({})
+
+
+@api_view(['GET'])
+@permission_classes((IsMemberUser, ))
+def get_article_sales(request, id):
+    # Endpoint qui permet d'obtenir, pour un article de pk {id}, le nombre de ventes.
+    article = perm_models.Article.objects.get(pk=id)
+    sessionid = request.session['payutc_session']
+    sales = article.update_sales(sessionid)
+    return JsonResponse({'sales': sales})
+
+
+
