9edd3d4dbd01f3a3b564323995de5046248e351b
Unknown <josselin.pennors@hotmail.fr>
2019-09-04 13:26:42 +0200

    Edit API Url

--- core/urls.py
+++ core/urls.py
@@ -17,23 +17,23 @@ urlpatterns = [
 	path('admin/', admin.site.urls),
 
 	# Authentication
-	path('api/auth/login', core_views.login, name='auth.login'),
-	path('api/auth/login_callback', core_views.login_callback, name='auth.login_callback'),
-	path('api/auth/badge', core_views.login_badge, name="auth.badge"),
-	path('api/auth/me', core_views.me, name='auth.me'),
-	path('api/auth/logout', core_views.logout, name='auth.logout'),
+	path('kraken/api/auth/login', core_views.login, name='auth.login'),
+	path('kraken/api/auth/login_callback', core_views.login_callback, name='auth.login_callback'),
+	path('kraken/api/auth/badge', core_views.login_badge, name="auth.badge"),
+	path('kraken/api/auth/me', core_views.me, name='auth.me'),
+	path('kraken/api/auth/logout', core_views.logout, name='auth.logout'),
 	
-	path('api/admin/settings', core_views.admin_settings),
-	path('api/core/user', core_views.user_information),
-	path('api/core/semester/state', core_views.semestre_state),
-	path('api/core/semester/credit', core_views.semester_beginning_credit),
-	path('api/current/semester', core_views.current_semester),
+	path('kraken/api/admin/settings', core_views.admin_settings),
+	path('kraken/api/core/user', core_views.user_information),
+	path('kraken/api/core/semester/state', core_views.semestre_state),
+	path('kraken/api/core/semester/credit', core_views.semester_beginning_credit),
+	path('kraken/api/current/semester', core_views.current_semester),
 
 	# Include other application urls
-	path('api/perms/', include('perm.urls')),
-	path('api/treso/', include('treso.urls')),
-	path('api/payutc/', include('payutc.urls')),
+	path('kraken/api/perms/', include('perm.urls')),
+	path('kraken/api/treso/', include('treso.urls')),
+	path('kraken/api/payutc/', include('payutc.urls')),
 
 	# Include routers
-	path('api/', include(router.urls)),
+	path('kraken/api/', include(router.urls)),
 ]
