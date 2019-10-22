adb91029628ce970de185678f940fe8b2e3618a1
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2019-10-22 11:55:31 +0200

    Create Survey models

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Survey(models.Model):
+
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="survey", null=True, blank=True, default=None)
+    description = models.TextField()
+
+
+class SurveyItem(models.Model):
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="survey", null=True, blank=True, default=None)
+    description = models.TextField()
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+
+class SurveyItemVote(models.Model):
+
+    login = models.CharField(max_length=10)
+    survey_item = models.ForeignKey(SurveyItem, on_delete=models.CASCADE)
