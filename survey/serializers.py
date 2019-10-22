4ca229077987d03c98a45ce71b4ed3b21d8de209
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2019-10-22 11:55:43 +0200

    Add serializers for survey models

---
+++ survey/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from . import models as survey_models
+
+class SurveySerializer(serializers.ModelSerializer):
+
+    survey_item_set = SurveyItemSerializer(many = True, read_only = True)
+
+    class Meta:
+        model = survey_models.Survey
+        exclude = list()
+
+
+class SurveyItemSerializer(serializers.ModelSerializer):
+    vote = serializers.PrimaryKeyRelatedField(queryset = survey_models.SurveyItemVote.objects.all())
+
+    class Meta:
+        model = survey_models.SurveyItem
+        fields = list()
+
+
+class SurveyItemVoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = survey_models.SurveyItemVote
+        fields = list()
