c98680c9b33237aa448c36f3795eedc17089588e
Josselin Pennors <33042009+jpennors@users.noreply.github.com>
2019-10-22 12:05:06 +0200

    Add survey views

--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from . import serializers as survey_serializers
+from .import models as survey_models
+from core.permissions import IsAdminUser, IsAuthenticatedUser, IsMemberUser, IsMemberUserOrReadOnly, CanAccessMenuFunctionnalities, HasApplicationRight
 
-# Create your views here.
+class SurveyViewSet(viewsets.ModelViewSet):
+    serializer_class = survey_serializers.SurveySerializer
+    queryset = survey_models.Survey.objects.all()
+    permission_classes = (IsMemberUser,)
+
+
+class SurveyItemViewSet(viewsets.ModelViewSet):
+    serializer_class = survey_serializers.SurveyItemSerializer
+    queryset = survey_models.SurveyItem.objects.all()
+    permission_classes = (IsMemberUser,)
+
+
+class SurveyItemVoteViewSet(viewsets.ModelViewSet):
+    serializer_class = survey_serializers.SurveyItemVoteSerializer
+    queryset = survey_models.SurveyItemVote.objects.all()
+    permission_classes = (IsMemberUser,)
