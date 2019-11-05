aab6e35292e99ad68cc09519ab3d77713a5c6b1b
Unknown <josselin.pennors@hotmail.fr>
2019-11-05 15:12:41 +0100

    Add methods to store and update img files

--- survey/models.py
+++ survey/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import os
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
+
 
 class Survey(models.Model):
 
@@ -8,6 +12,38 @@ class Survey(models.Model):
     visible = models.BooleanField(default=False)
     multi_choice = models.BooleanField(default=False)
 
+@receiver(models.signals.post_delete, sender=Survey)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+
+@receiver(models.signals.pre_save, sender=Survey)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Survey.objects.get(pk=instance.pk).image
+    except Survey.DoesNotExist:
+        return False
+    print(old_file)
+    new_file = instance.image
+    print(new_file)
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
+
 
 class SurveyItem(models.Model):
 
@@ -15,6 +51,38 @@ class SurveyItem(models.Model):
     image = models.ImageField(upload_to="survey", null=True, blank=True, default=None)
     description = models.TextField()
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+@receiver(models.signals.post_delete, sender=SurveyItem)
+def auto_delete_file_item_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+
+@receiver(models.signals.pre_save, sender=SurveyItem)
+def auto_delete_file_item_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = SurveyItem.objects.get(pk=instance.pk).image
+    except SurveyItem.DoesNotExist:
+        return False
+    print(old_file)
+    new_file = instance.image
+    print(new_file)
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
 
 
 class SurveyItemVote(models.Model):
