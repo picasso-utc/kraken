75e4ed2f75b69b54953169f705d4314dc575a715
Unknown <josselin.pennors@hotmail.fr>
2019-11-05 15:04:51 +0100

    Create view to get public result for a survey

--- survey/views.py
+++ survey/views.py
@@ -32,3 +32,36 @@ def get_public_surveys(request):
         for item in survey['surveyitem_set']:
             del item["surveyitemvote_set"]
     return JsonResponse({'surveys' : surveys})
+@api_view(['GET'])
+@permission_classes((IsAuthenticatedUser, ))
+def get_survey_results(request, id=None):
+    login = request.session['login']
+    queryset = survey_models.Survey.objects.filter(visible=True, id=id)
+    serializer = survey_serializers.SurveySerializer(queryset, many=True)
+    surveys = serializer.data
+    if len(surveys) > 0:
+        survey = surveys[0]
+        votes = 0
+        results = []
+        found = False
+        for item in survey['surveyitem_set']:
+            votes += len(item['surveyitemvote_set'])
+            item["votes"] = len(item['surveyitemvote_set'])
+            vote_login = [v for v in item['surveyitemvote_set'] if v['login'] == login]
+            if len(vote_login) > 0:
+                found = True
+                item["voted"] = True
+            else :
+                item["voted"] = False
+            del item['surveyitemvote_set']
+
+        for item in survey['surveyitem_set']:
+            if votes > 0:
+                result = {'id': item["id"], "votes": round((item["votes"]/votes) * 100,2)}
+                results.append(result)
+
+    if found:
+        return JsonResponse({'results' : results})
+    else :
+        return JsonResponse({'error': 'Vous n\'avez pas participé à ce sondage'}, status = 409)
+@api_view(['GET'])
