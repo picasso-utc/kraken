a4cbc79fd767d18ef98a64e22a3fe12091689a5c
Unknown <josselin.pennors@hotmail.fr>
2019-11-12 17:54:11 +0100

    Rework on TV serializer

--- tv/serializers.py
+++ tv/serializers.py
@@ -3,20 +3,22 @@ from tv import models as tv_models
 
 
 class WebTVLinkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = tv_models.WebTVLink
-        exclude = list()
+	url = serializers.URLField()
+	class Meta:
+		model = tv_models.WebTVLink
+		exclude = list()
 
 
 class WebTVSerializer(serializers.ModelSerializer):
 	link = WebTVLinkSerializer(read_only=True) 
-	link_id = serializers.PrimaryKeyRelatedField(queryset=tv_models.WebTVLink.objects.all())
+	link_id = serializers.PrimaryKeyRelatedField(queryset=tv_models.WebTVLink.objects.all(), source="link")
 	class Meta:
 		model = tv_models.WebTV
 		exclude = list()
 
 
 class WebTVMediaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = tv_models.WebTVMedia
-        exclude = list()
+	media = serializers.FileField(allow_empty_file=True, allow_null=True, use_url=False)
+	class Meta:
+		model = tv_models.WebTVMedia
+		exclude = list()
