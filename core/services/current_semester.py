a134edae2a3f474d05dd36773d3bd7f46eb43106
Unknown <josselin.pennors@hotmail.fr>
2019-12-31 12:20:21 +0100

    Add possibility to edit filter attribute

--- core/services/current_semester.py
+++ core/services/current_semester.py
@@ -4,13 +4,16 @@ def get_current_semester():
     return live_config.SEMESTER
 
 
-def get_request_semester(qs, request=None):
+def get_request_semester(qs, request=None, attribute=None):
+    if not attribute:
+        attribute = "semestre__id"
     if request:
         semester_wanted = request.GET.get("semestre", False)
     if request and semester_wanted != False:
         if semester_wanted == "all":
             return qs.all()
         elif int(semester_wanted) > 0:
-            return qs.filter(semestre__id=int(semester_wanted))
+            return qs.filter(**{attribute: int(semester_wanted)})
+            
     else:
-        return qs.filter(semestre__id=live_config.SEMESTER)
+        return qs.filter(**{attribute: live_config.SEMESTER})
