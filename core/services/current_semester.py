864f3673ce75fae69206d4c1131beced74888e20
Unknown <josselin.pennors@hotmail.fr>
2019-12-31 11:42:19 +0100

    Add method to get request semester

--- core/services/current_semester.py
+++ core/services/current_semester.py
@@ -2,3 +2,15 @@ from constance import config as live_config
 
 def get_current_semester():
     return live_config.SEMESTER
+
+
+def get_request_semester(qs, request=None):
+    if request:
+        semester_wanted = request.GET.get("semestre", False)
+    if request and semester_wanted != False:
+        if semester_wanted == "all":
+            return qs.all()
+        elif int(semester_wanted) > 0:
+            return qs.filter(semestre__id=int(semester_wanted))
+    else:
+        return qs.filter(semestre__id=live_config.SEMESTER)
