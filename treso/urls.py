6952ce25b6eb2c40e275361cb25543d6a7b2f040
Unknown <josselin.pennors@hotmail.fr>
2020-02-06 10:28:06 +0100

    fix issue with url

--- treso/urls.py
+++ treso/urls.py
@@ -6,6 +6,6 @@ from treso import views as treso_views
 urlpatterns = [
 	path('tvainfo/<int:id>', treso_views.tva_info),
 	path('convention/<int:id>', treso_views.get_convention),
-	path('conventions', treso_views.get_all_conventions)
+	path('conventions', treso_views.get_all_conventions),
 	path('excel/factures', treso_views.excel_facture_generation)
 ]
