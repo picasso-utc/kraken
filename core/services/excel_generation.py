431381016085eeaffc5dc8012d0feff20c9c0f4c
Unknown <josselin.pennors@hotmail.fr>
2020-02-06 11:57:15 +0100

    fix issues with service invoice excel, add perm information

--- core/services/excel_generation.py
+++ core/services/excel_generation.py
@@ -1,6 +1,8 @@
 # coding: utf8
 
 from treso import models as treso_models
+from core.services.current_semester import get_request_semester
+from treso.serializers import FactureRecueExtendedSerializer
 
 def generate_checks_xls(worksheet):
     qs = list(treso_models.Cheque.objects.all().order_by('num').prefetch_related('facturerecue'))
@@ -18,26 +20,34 @@ def generate_checks_xls(worksheet):
             worksheet.write(num+1, 5, "--")
     return worksheet
 
-def generate_receipts_xls(worksheet):
-    qs = list(treso_models.FactureRecue.objects.all().order_by('id').prefetch_related('perm')
-              .prefetch_related('categorie'))
+
+def generate_receipts_xls(worksheet, request):
+    qs = treso_models.FactureRecue.objects
+    qs = list(get_request_semester(qs, request).order_by('id'))
     for n, val in enumerate(["Référence", "Entreprise", "Date", "Date de paiement", "Prix TTC", "TVA", "Etat",
                              "Personne à rembourser", "Date de remboursement", "Nom de la perm", "Date de la perm",
                              "Période de la perm", "Responsable de la perm"]):
         worksheet.write(0, n, val)
     for num, facture in enumerate(qs):
+        serializer = FactureRecueExtendedSerializer(facture)
         for n, val in enumerate([facture.categorie.code + str(facture.id) if facture.categorie else facture.id,
-                                 facture.nom_entreprise, unicode(facture.date or "--"),
-                                 unicode(facture.date_paiement or "--"), facture.prix,
+                                 facture.nom_entreprise, str(facture.date or "--"),
+                                 str(facture.date_paiement or "--"), facture.prix,
                                  facture.get_total_taxes(), facture.get_etat_display(),
-                                 facture.personne_a_rembourser or "", facture.date_remboursement or ""]):
+                                 facture.personne_a_rembourser or "", str(facture.date_remboursement) or ""]):
             worksheet.write(num+1, n, val)
         if facture.perm:
-            worksheet.write(num+1, 9, facture.perm.nom)
-            worksheet.write(num + 1, 10, unicode(facture.perm.date))
-            worksheet.write(num + 1, 11, facture.perm.get_periode_display())
-            worksheet.write(num + 1, 12, (facture.perm.nom_resp or "") + " - " + (facture.perm.mail_resp or "Pas d'email"))
+            worksheet.write(num+1, 9, facture.perm.perm.nom)
+            worksheet.write(num + 1, 10, str(facture.perm.date))
+            if facture.perm.creneau == "M":
+                worksheet.write(num + 1, 11, "Matin")
+            elif facture.perm.creneau == "D":
+                worksheet.write(num + 1, 11, "Midi")
+            else:
+                worksheet.write(num + 1, 11, "Soir")
+            worksheet.write(num + 1, 12, (facture.perm.perm.nom_resp or "") + " - " + (facture.perm.perm.mail_resp or "Pas d'email"))
         else:
             for i in range(9, 13):
                 worksheet.write(num+1, i, "--")
     return worksheet
+
